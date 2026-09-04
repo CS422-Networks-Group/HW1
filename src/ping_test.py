19a2a09c85f73099aff9deb6f4fd601adedab445
rgangatirkar1 <raghav.gangatirkar@gmail.com>
2026-09-04 15:47:15 -0400

    added home ip + distance in miles

--- src/ping_test.py
+++ src/ping_test.py
@@ -9,6 +9,8 @@ import IP2Location
 import os
 from tqdm import tqdm
 import threading
+from requests import get
+from geopy.distance import geodesic
 
 #check whether a value is an ip address or a host name
 def is_ip_address(value: str) -> bool:
@@ -33,8 +35,6 @@ def process_csv(csv_file: str) -> pd.DataFrame:
         ip_or_host = row["IP/HOST"]
         valid_ip = True
         if not is_ip_address(ip_or_host):
-            
-            #TODO: n    eed to add our computer's ip addresses when the script is ran
             try:
                 ip_or_host = socket.gethostbyname(ip_or_host)
             except socket.gaierror:
@@ -48,11 +48,18 @@ def process_csv(csv_file: str) -> pd.DataFrame:
         
         #for now, drop the rows whose latitude and longitutde fields are empty
     filtered_df = df[df["LATITUDE"].notna() & df["LONGITUDE"].notna()]
+    ip = get('https://api.ipify.org').content.decode('utf8')
+    df.loc[len(df)] = {
+        "IP/HOST": ip,
+        "LATITUDE": database.get_all(ip).latitude,
+        "LONGITUDE": database.get_all(ip).longitude,
+    }
+    
 
     return filtered_df
 
 
-def execute_ping_tests(df: pd.DataFrame, start: int, end: int):
+def execute_ping_tests(df: pd.DataFrame, start: int, end: int, result_df):
     for _, row in df.iloc[start:end+1].iterrows():
         res = subprocess.run(
             ["ping", "-c", "11", row["IP/HOST"]],
@@ -75,11 +82,27 @@ def main():
     plot it
     '''
     df = process_csv("data/listed_iperf3_servers.csv")
-    thread1 = threading.Thread(target=execute_ping_tests, args=(df, 0, 37))
-    thread2 = threading.Thread(target=execute_ping_tests, args=(df, 38, 75))
-    thread3 = threading.Thread(target=execute_ping_tests, args=(df, 76, 113))
-    thread4 = threading.Thread(target=execute_ping_tests, args=(df, 114, 151))
-    thread5 = threading.Thread(target=execute_ping_tests, args=(df, 152, 188))
+    result_df = pd.DataFrame(index=range(df.shape[0]), columns=df.columns)
+    result_df["IP_ADDRESS"] = None
+    result_df["MIN RTT"] = None
+    result_df["MAX RTT"] = None
+    result_df["AVERAGE RTT"] = None
+    result_df["DISTANCE"] = None
+
+    for index, row in df.iterrows():
+        result_df.loc[index,"IP_ADDRESS"] = row["IP/HOST"]
+        home_ip = (df.iloc[-1]["LATITUDE"], df.iloc[-1]["LONGITUDE"])
+        dest_ip = (row["LATITUDE"], row["LONGITUDE"])
+        result_df.loc[index,"DISTANCE"] = geodesic(home_ip, dest_ip).miles
+        
+
+
+
+    thread1 = threading.Thread(target=execute_ping_tests, args=(df, 0, 37, result_df))
+    thread2 = threading.Thread(target=execute_ping_tests, args=(df, 38, 75, result_df))
+    thread3 = threading.Thread(target=execute_ping_tests, args=(df, 76, 113, result_df))
+    thread4 = threading.Thread(target=execute_ping_tests, args=(df, 114, 151, result_df))
+    thread5 = threading.Thread(target=execute_ping_tests, args=(df, 152, 188, result_df))
 
     thread1.start()
     thread2.start()
@@ -93,7 +116,6 @@ def main():
     thread4.join()
 
     thread5.join()
-
 
 if __name__ == "__main__":
     main()
