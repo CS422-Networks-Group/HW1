f44d0f8baf2f15a64b53a55fc12cf088354b8f9f
rgangatirkar1 <raghav.gangatirkar@gmail.com>
2026-09-04 15:02:42 -0400

    added concurrency

--- src/ping_test.py
+++ src/ping_test.py
@@ -7,6 +7,8 @@ import ipaddress
 import subprocess
 import IP2Location  
 import os
+from tqdm import tqdm
+import threading
 
 #check whether a value is an ip address or a host name
 def is_ip_address(value: str) -> bool:
@@ -49,6 +51,15 @@ def process_csv(csv_file: str) -> pd.DataFrame:
 
     return filtered_df
 
+
+def execute_ping_tests(df: pd.DataFrame, start: int, end: int):
+    for _, row in df.iloc[start:end+1].iterrows():
+        res = subprocess.run(
+            ["ping", "-c", "11", row["IP/HOST"]],
+            capture_output=True,
+            text=True
+        )
+        print(res.stdout)
 def main():
     '''
     basic flow: read the csv
@@ -64,12 +75,25 @@ def main():
     plot it
     '''
     df = process_csv("data/listed_iperf3_servers.csv")
-    
-    #running ping tests
-    for _, row in df.iterrows():
-        res = subprocess.run(["ping", "-c", "11", row["IP/HOST"]], capture_output=True, text=True)
-        print(res)
-    return True
+    thread1 = threading.Thread(target=execute_ping_tests, args=(df, 0, 37))
+    thread2 = threading.Thread(target=execute_ping_tests, args=(df, 38, 75))
+    thread3 = threading.Thread(target=execute_ping_tests, args=(df, 76, 113))
+    thread4 = threading.Thread(target=execute_ping_tests, args=(df, 114, 151))
+    thread5 = threading.Thread(target=execute_ping_tests, args=(df, 152, 188))
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
+    thread5.start()
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    thread4.join()
+
+    thread5.join()
+
 
 if __name__ == "__main__":
     main()
